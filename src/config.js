fd94baf130e19e2494f3d1955c060e38135c1810
oneheka <oneheka.com@gmail.com>
2025-01-09 01:44:42 +0700

    readme

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ export const github = 'https://github.com/oneheka' // Your github url
 export const theme = 'dark' // "dark" or "light"
 
 export const toaster = {
-    enabled: false, // State toaster visible
-    title: '', // Title your socail network ("Telegram channel", "My youtube channel" and other...)
-    url: '' // Your link to social network ("https://t.me/yourusername", "https://www.youtube.com/@yourusername" and other...)
+    enabled: true, // State toaster visible
+    title: 'Telegram channel', // Title your socail network ("Telegram channel", "My youtube channel" and other...)
+    url: 'https://t.me/yourusername' // Your link to social network ("https://t.me/yourusername", "https://www.youtube.com/@yourusername" and other...)
 }
